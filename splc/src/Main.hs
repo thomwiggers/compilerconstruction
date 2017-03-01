f6f21f3138cdf1fb84fe8df3c0931df7bf19cf84
Thom Wiggers <thom@thomwiggers.nl>
2017-03-01 12:47:08 +0100

    prettyprinter

--- splc/src/Main.hs
+++ splc/src/Main.hs
@@ -1,39 +1,61 @@
 module Main (main) where
 
-import Options.Applicative
+import Options.Applicative (subparser, Parser, info, execParser, fullDesc,
+        helper, progDesc, (<**>), command, (<|>), argument, str, help,
+        flag', short, long, metavar)
 import Data.Semigroup ((<>))
 
+import Text.Megaparsec (runParser, parseErrorPretty)
+import SplParser (spl)
+import SplPrettyPrinter
+
 data InputSrc = StdInput | Filename String
+    deriving (Eq, Show)
 
-data ArgOptions = ArgOptions {
-    pretty_print :: Bool,
-    input :: InputSrc
-    }
+data Command = PrettyPrint InputSrc
+    deriving (Eq, Show)
 
-argOptions :: Parser ArgOptions
-argOptions = ArgOptions
-    <$> prettyPrintSwitch
-    <*> (parseFile <|> parseStdIn)
+commandParser :: Parser Command
+commandParser = subparser
+    (command "pretty" (info prettyPrintCommand (progDesc "pretty printer")))
 
-prettyPrintSwitch :: Parser Bool
-prettyPrintSwitch = switch $
-       long "pretty-print"
-    <> short 'p'
-    <> help "run the pretty printer"
+prettyPrintCommand :: Parser Command
+prettyPrintCommand =  PrettyPrint <$> (parseFile <|> parseStdIn)
+
 
 parseFile :: Parser InputSrc
-parseFile = Filename <$> strOption (
+parseFile = Filename <$> argument str (
       metavar "FILE"
    <> help "filename")
 
 parseStdIn :: Parser InputSrc
 parseStdIn = flag' StdInput (short 'i' <> long "stdin")
 
+doPrettyPrint :: InputSrc -> IO ()
+doPrettyPrint src = do
+    fileContent <- case src of
+            StdInput -> getContents
+            Filename f -> readFile f
+
+    let filename = case src of
+            StdInput   -> "stdin"
+            Filename f -> f
+
+    let parsed = runParser spl filename fileContent
+
+    case parsed of
+            Left err -> putStr (parseErrorPretty err)
+            Right ast -> putStrLn (pprint ast)
+
+
+
 main :: IO ()
 main = do
     opts <- execParser theOpts
-    print "hello world"
+    case opts of
+        (PrettyPrint src) -> doPrettyPrint src
+
     where
-        theOpts = info (argOptions <**> helper) $
-               fullDesc 
+        theOpts = info (commandParser <**> helper) $
+               fullDesc
             <> progDesc "SPL compiler, pretty-printer and parser"
