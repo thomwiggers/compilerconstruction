d6e754c26abace17d196ee9cd3ad0146382ab62a
Thom Wiggers <thom@thomwiggers.nl>
2017-03-30 10:38:30 +0200

    Typecheck functions, unary expr, binary expr

--- splc/src/SplTypeChecker.hs
+++ splc/src/SplTypeChecker.hs
@@ -4,6 +4,7 @@ module SplTypeChecker where
 import qualified Data.Map as Map
 
 import Prelude hiding (fail)
+import Data.List
 import Control.Monad (when)
 import Control.Monad.Fail
 import Control.Monad.Trans.State
@@ -20,14 +21,11 @@ data SplSimpleTypeR = SplTypeConst SplBasicType
                     | SplTypeTupleR SplSimpleTypeR SplSimpleTypeR
                     | SplTypeListR SplSimpleTypeR
                     | SplTypeVar Int
-        deriving (Show, Eq)
-
-data SplReturnTypeR = SplRetTypeR SplSimpleTypeR
-                    | SplRetVoidR
+                    | SplVoid
         deriving (Show, Eq)
 
 data SplTypeR = SplSimple SplSimpleTypeR
-              | SplTypeFunction [SplSimpleTypeR] SplReturnTypeR
+              | SplTypeFunction [SplSimpleTypeR] SplSimpleTypeR
         deriving (Show, Eq)
 
 emptyEnvironment :: Environment
@@ -43,15 +41,23 @@ unsimple (SplTypeFunction _ _) = fail "Unexpected function type"
 returnSimple :: SplSimpleTypeR -> SplTypeCheckResult
 returnSimple x = return (SplSimple x)
 
+disallowVoid :: SplSimpleTypeR -> StateT Environment (Either String) SplSimpleTypeR
+disallowVoid (SplVoid) = fail "Void is not allowed"
+disallowVoid t = return t
+
+notVoid :: SplSimpleTypeR -> Bool
+notVoid SplVoid = False
+notVoid _       = True
+
 instance SplTypeChecker SplType where
     typeCheck SplTypeUnknown = fail "Not allowed yet"
     typeCheck (SplTypePlaceholder _) = fail "Not allowed yet"
     typeCheck (SplTypeTuple l r) = do
-        lt <- typeCheck l >>= unsimple
-        rt <- typeCheck r >>= unsimple
+        lt <- typeCheck l >>= unsimple >>= disallowVoid
+        rt <- typeCheck r >>= unsimple >>= disallowVoid
         returnSimple (SplTypeTupleR lt rt)
     typeCheck (SplTypeList a) = do
-        at <- typeCheck a >>= unsimple
+        at <- typeCheck a >>= unsimple >>= disallowVoid
         returnSimple (SplTypeListR at)
     typeCheck (SplType t) = returnSimple $ SplTypeConst t
 
@@ -59,7 +65,8 @@ instance SplTypeChecker SplVarDecl where
     typeCheck (SplVarDecl varType name expr) = do
         t <- typeCheck varType
         e <- typeCheck expr
-        if (t == e) 
+        _ <- unsimple e >>= disallowVoid
+        if (t == e)
             then (do
                 declared <- gets (Map.member name)
                 when declared
@@ -69,20 +76,115 @@ instance SplTypeChecker SplVarDecl where
             )
             else fail "Type mismatch when parsing varDecl"
 
+typeCheckField :: SplSimpleTypeR -> SplField -> SplTypeCheckResult
+typeCheckField t SplFieldNone = returnSimple t
+typeCheckField (SplTypeListR t) (SplFieldHd f) = typeCheckField t f
+typeCheckField t@(SplTypeListR _) (SplFieldTl f) = typeCheckField t f
+typeCheckField (SplTypeTupleR l _) (SplFieldFst f) = typeCheckField l f
+typeCheckField (SplTypeTupleR _ r) (SplFieldSnd f) = typeCheckField r f
+typeCheckField _ (SplFieldHd _) = fail "Cannot take hd of a non-list"
+typeCheckField _ (SplFieldTl _) = fail "Cannot take tl of a non-list"
+typeCheckField _ (SplFieldFst _) = fail "Cannot take fst of a non-tuple"
+typeCheckField _ (SplFieldSnd _) = fail "Cannot take snd of a non-tuple"
+
+
 instance SplTypeChecker SplExpr where
     typeCheck (SplIntLiteralExpr _) = returnSimple $ SplTypeConst SplInt
     typeCheck (SplBooleanLiteralExpr _) = returnSimple $ SplTypeConst SplBool
     typeCheck (SplCharLiteralExpr _) = returnSimple $ SplTypeConst SplChar
-    typeCheck SplEmptyListExpr = fail "huh"
-    typeCheck (SplIdentifierExpr name SplFieldNone) = do
+    typeCheck SplEmptyListExpr = fail "empty lists are weird without inference"
+
+    typeCheck (SplTupleExpr l r) = do
+            lType <- typeCheck l >>= unsimple >>= disallowVoid
+            rType <- typeCheck r >>= unsimple >>= disallowVoid
+            -- FIXME functions in tuples
+            returnSimple $ SplTypeTupleR lType rType
+
+    typeCheck (SplIdentifierExpr name field) = do
         varType <- gets (Map.lookup name)
+        -- todo check if it's a function
         case varType of
-            Just t -> return t
+            Just t -> unsimple t >>= (flip typeCheckField field)
             Nothing -> fail $ "Undeclared variable " ++ name
+
+    typeCheck (SplUnaryExpr op expr) = do
+        exprType <- typeCheck expr >>= unsimple >>= disallowVoid
+        case op of
+            SplOperatorInvert -> if (exprType == SplTypeConst SplBool)
+                then returnSimple exprType
+                else fail "Cannot invert a non-Bool"
+            SplOperatorNegate -> if (exprType == SplTypeConst SplInt)
+                then returnSimple exprType
+                else fail "Cannot invert a non-Int"
+
+    -- TODO fix cons: it has t × [t] -> t instead of t × t -> t
+    typeCheck (SplBinaryExpr SplOperatorCons _ _) = fail "FIXME"
+
+    typeCheck (SplBinaryExpr op e1 e2) = do
+        e1Type <- typeCheck e1 >>= unsimple >>= disallowVoid
+        e2Type <- typeCheck e2 >>= unsimple >>= disallowVoid
+        when (e1Type /= e2Type) (
+            fail $ "Both operands need to be of the same type\n"
+                ++ "Actual: " ++ (show e1Type) ++ " " ++ (show op)
+                ++ " " ++ (show e2Type))
+        typeCheckBinOp op e1Type
+
     typeCheck (SplFuncCallExpr name args) = do
         funcType <- gets (Map.lookup name)
         case funcType of
-            Just t -> return t
-            Nothing -> fail $ "Undeclared variable " ++ name
+            Nothing -> fail $ "Undeclared function name " ++ name
+            Just (SplSimple _) -> fail "Expected function, got variable"
+            Just (SplTypeFunction argTypes retType) -> (do
+                    when (length args /= length argTypes) (
+                            fail ("Got " ++ (show . length) args ++
+                                  " arguments but expected " ++ (show . length) argTypes))
+                    argTypeResults <- mapM typeCheck args
+                    let result = all checkArgType (zip argTypes argTypeResults)
+                    if result
+                        then returnSimple retType
+                        else fail ("Error checking arguments for " ++ name ++ ":\n"
+                                     ++ "Expected: " ++ (intercalate ", " (map show args)) ++ "\n"
+                                     ++ "Got:      " ++ (intercalate ", " (map show argTypes)))
 
-    typeCheck _ = fail "Not implemented"
+                )
+        where
+            checkArgType :: (SplSimpleTypeR, SplTypeR) -> Bool
+            checkArgType (expected, SplSimple actual) = actual == expected && notVoid actual
+            checkArgType _ = False
+
+typeCheckBinOp :: SplBinaryOperator -> SplSimpleTypeR -> SplTypeCheckResult
+typeCheckBinOp SplOperatorAdd t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorAdd t@(SplTypeConst SplChar) = returnSimple t
+
+typeCheckBinOp SplOperatorSubtract t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorSubtract t@(SplTypeConst SplChar) = returnSimple t
+
+typeCheckBinOp SplOperatorMultiply t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorDivide t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorModulus t@(SplTypeConst SplInt) = returnSimple t
+
+typeCheckBinOp SplOperatorLess t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorLess t@(SplTypeConst SplChar) = returnSimple t
+
+typeCheckBinOp SplOperatorLessEqual t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorLessEqual t@(SplTypeConst SplChar) = returnSimple t
+
+typeCheckBinOp SplOperatorEqual t@(SplTypeConst _) = returnSimple t
+typeCheckBinOp SplOperatorEqual t@(SplTypeTupleR _ _) = returnSimple t
+typeCheckBinOp SplOperatorEqual t@(SplTypeListR _) = returnSimple t
+
+typeCheckBinOp SplOperatorGreaterEqual t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorGreaterEqual t@(SplTypeConst SplChar) = returnSimple t
+
+typeCheckBinOp SplOperatorGreater t@(SplTypeConst SplInt) = returnSimple t
+typeCheckBinOp SplOperatorGreater t@(SplTypeConst SplChar) = returnSimple t
+
+typeCheckBinOp SplOperatorNotEqual t@(SplTypeConst _) = returnSimple t
+typeCheckBinOp SplOperatorNotEqual t@(SplTypeTupleR _ _) = returnSimple t
+typeCheckBinOp SplOperatorNotEqual t@(SplTypeListR _) = returnSimple t
+
+typeCheckBinOp SplOperatorAnd t@(SplTypeConst SplBool) = returnSimple t
+typeCheckBinOp SplOperatorOr t@(SplTypeConst SplBool) = returnSimple t
+
+typeCheckBinOp SplOperatorCons _ = fail "Don't use typeCheckBinOp on Cons, dummy"
+typeCheckBinOp o t = fail $ "Unsupported operands of type " ++ (show t) ++ " for operator " ++ (show o)
