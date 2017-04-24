f4f33147479421981a48db63c637b11199fed39e
Thom Wiggers <thom@thomwiggers.nl>
2017-04-24 15:24:38 +0200

    Allow simple recursion

--- splc/src/SplTypeChecker.hs
+++ splc/src/SplTypeChecker.hs
@@ -258,28 +258,34 @@ instance Inferer SplDecl where
         let funExists = Map.member (Var, name) env
         when (funExists) (throwError $ ConflictingDeclaration name)
 
-        -- copy state for scope resolution
-        globalState <- get
-
-        -- Assign arguments with their types
         nameTypes <- if (null argTypes)
-                        then do
-                            argTypes' <- replicateM (length argNames) fresh
-                            return $ zip argNames argTypes'
-                        else do
-                            -- we argue that substitutions on types in the function params don't matter at this point
-                            argTypes' <- mapM (\x -> infer x >>= unsimple >>= \(_, y) -> return y) argTypes
-                            return $ zip argNames argTypes'
-
-        -- insert all arguments into the local environment
-        -- fixme check if forall t -> t is needed
-        let localEnv = foldr (\(n, t) -> Map.insert (Var, n) (Forall [] (SplSimple t))) env nameTypes
+            then do
+                argTypes' <- replicateM (length argNames) fresh
+                return $ zip argNames argTypes'
+            else do
+                -- we argue that substitutions on types in the function params don't matter at this point
+                argTypes' <- mapM (\x -> infer x >>= unsimple >>= \(_, y) -> return y) argTypes
+                return $ zip argNames argTypes'
 
         -- set up expected return value
         (_, retType') <- infer retType >>= unsimple
 
+        -- insert function declaration into state
+        let argTypes' = snd $ unzip nameTypes
+        let resultType = SplTypeFunction argTypes' retType'
+        modify $ extend ((Var, name), Forall (Set.toList $ ftv argTypes') resultType)
+
+        -- copy state for scope resolution
+        globalState <- get
+
+        -- Assign arguments with their types
+        -- insert all arguments into the local environment
+        -- fixme check if forall t -> t is needed
+        let (TypeEnv env') = typeEnv globalState
+        let localEnv = foldr (\(n, t) -> Map.insert (Var, n) (Forall [] (SplSimple t))) env' nameTypes
+
         -- prepare local env, push false on return stack, set expected return type
-        let localState = globalState{typeEnv = TypeEnv localEnv, returnBlocks = [False], returnType = retType' }
+        let localState = globalState{typeEnv = TypeEnv localEnv, returnBlocks = [], returnType = retType' }
         put localState
 
         -- check var decls
@@ -301,10 +307,6 @@ instance Inferer SplDecl where
         -- apply substitutions
         modify $ apply (s `compose` subVars)
 
-        -- insert function declaration into state
-        let argTypes' = snd $ unzip nameTypes
-        let resultType = SplTypeFunction argTypes' retType'
-        modify $ extend ((Var, name), apply (s `compose` subVars) (Forall (Set.toList $ ftv argTypes') resultType))
 
         -- return type
         return ((s `compose` subVars), resultType)
@@ -400,8 +402,8 @@ instance Inferer SplExpr where
             binaryOp argType retType = do
                 (sl, tl) <- infer l >>= unsimple
                 (sr, tr) <- infer r >>= unsimple
-                sl' <- unify tl $ SplTypeConst argType
-                sr' <- unify tr $ SplTypeConst argType
+                sl' <- unify (SplTypeConst argType) tl
+                sr' <- unify (SplTypeConst argType) tr
                 returnSimple (sr' `compose` sl' `compose` sr `compose` sl) $ SplTypeConst retType
 
             catchBinaryCharOp retType (UnificationFail _ c) = do
@@ -583,187 +585,3 @@ inferField parentType (SplFieldSnd f) = do
     s <- unify (SplTypeTupleR n m) parentType
     modify $ apply s
     inferField (apply s m) f
-
-{-
-class SplTypeChecker a where
-    typeCheck :: a -> SplTypeCheckResult
-
-instance SplTypeChecker SplDecl where
-    typeCheck (SplDeclVar varDecl) = typeCheck varDecl
-
-    typeCheck (SplDeclFun name argNames argTypes retType varDecls stmts) = do
-        declared <- gets (Map.member name)
-        when declared
-            (fail $ "Variable " ++ name ++ " already declared")
-
-        when (nub argNames /= argNames) $
-            fail $ "Duplicate argument names in function declaration " ++ name
-
-        checkedArgTypes <- mapM (\x -> typeCheck x >>= unsimple) argTypes
-        localState <- get
-        globalState <- get
-
-        let nameArgs = zip argNames (map SplSimple checkedArgTypes)
-        let localState' = foldr (\(s, n) -> Map.insert s n) localState nameArgs
-        put localState'
-
-        mapM_ (\x -> typeCheck x >>= unsimple) varDecls
-
-        retType' <- case retType of
-            SplRetType t ->  typeCheck t >>= unsimple
-            SplRetVoid -> return SplVoid
-
-        blockType <- typeCheck stmts >>= unsimple
-
-        when (blockType /= retType') $
-            fail $ "Cannot unify return type " ++ (show retType) ++
-                " with actual type " ++ (show blockType) ++ "."
-
-        let functionType = SplTypeFunction checkedArgTypes retType'
-        put (Map.insert name functionType globalState)
-
-        return functionType
-
-instance SplTypeChecker SplStmt where
-    typeCheck (SplIfStmt cond thenStmts elseStmts) = do
-        condType <- typeCheck cond >>= unsimple
-        when (condType /= SplTypeConst SplBool) $
-            fail $ "Condition needs to be a Bool instead of a " ++ (show condType)
-        typeCheck (thenStmts ++ elseStmts)
-
-    typeCheck (SplWhileStmt cond stmts) = do
-        condType <- typeCheck cond >>= unsimple
-        when (condType /= SplTypeConst SplBool) $
-            fail $ "Condition needs to be a Bool instead of a " ++ (show condType)
-        typeCheck stmts
-
-    typeCheck (SplAssignmentStmt name field expr) = do
-        varType' <- gets (Map.lookup name)
-        varType <- case varType' of
-            Just t -> unsimple t >>= (flip typeCheckField field) >>= unsimple
-            Nothing -> fail $ "Undeclared variable " ++ name
-        exprType <- typeCheck expr >>= unsimple >>= disallowVoid
-        if (exprType == varType)
-            then returnSimple SplVoid
-            else fail $ "Cannot assign result of type " ++ (show exprType) ++
-                    " to variable of type " ++ (show varType) ++ "."
-
-    typeCheck (SplFuncCallStmt name args) = checkFunctionCall name args
-
-    typeCheck (SplReturnStmt e) =
-            typeCheck e >>= unsimple >>= disallowVoid >>= returnSimple
-
-    typeCheck SplReturnVoidStmt = returnSimple SplVoid
-
-
-instance SplTypeChecker SplExpr where
-    typeCheck (SplIntLiteralExpr _) = returnSimple $ SplTypeConst SplInt
-    typeCheck (SplBooleanLiteralExpr _) = returnSimple $ SplTypeConst SplBool
-    typeCheck (SplCharLiteralExpr _) = returnSimple $ SplTypeConst SplChar
-    typeCheck SplEmptyListExpr = fail "empty lists are weird without inference"
-
-    typeCheck (SplTupleExpr l r) = do
-            lType <- typeCheck l >>= unsimple >>= disallowVoid
-            rType <- typeCheck r >>= unsimple >>= disallowVoid
-            -- FIXME functions in tuples
-            returnSimple $ SplTypeTupleR lType rType
-
-    typeCheck (SplIdentifierExpr name field) = do
-        varType <- gets (Map.lookup name)
-        -- todo check if it's a function
-        case varType of
-            Just t -> unsimple t >>= (flip typeCheckField field)
-            Nothing -> fail $ "Undeclared variable " ++ name
-
-    typeCheck (SplUnaryExpr op expr) = do
-        exprType <- typeCheck expr >>= unsimple >>= disallowVoid
-        case op of
-            SplOperatorInvert -> if (exprType == SplTypeConst SplBool)
-                then returnSimple exprType
-                else fail "Cannot invert a non-Bool"
-            SplOperatorNegate -> if (exprType == SplTypeConst SplInt)
-                then returnSimple exprType
-                else fail "Cannot invert a non-Int"
-
-    typeCheck (SplBinaryExpr SplOperatorCons e1 e2) = do
-        e1Type <- typeCheck e1 >>= unsimple >>= disallowVoid
-        e2Type <- typeCheck e2 >>= unsimple >>= disallowVoid
-        case e2Type of
-            SplTypeListR t2 -> if (e1Type == t2)
-                then returnSimple e2Type
-                else fail $ "Expected list of type " ++ (show e1Type)
-            _ -> fail "Cons expects a list as second argument"
-
-    typeCheck (SplBinaryExpr op e1 e2) = do
-        e1Type <- typeCheck e1 >>= unsimple >>= disallowVoid
-        e2Type <- typeCheck e2 >>= unsimple >>= disallowVoid
-        when (e1Type /= e2Type) (
-            fail $ "Both operands need to be of the same type\n"
-                ++ "Actual: " ++ (show e1Type) ++ " " ++ (show op)
-                ++ " " ++ (show e2Type))
-        typeCheckBinOp op e1Type
-
-    typeCheck (SplFuncCallExpr name args) = checkFunctionCall name args
-
-
-checkFunctionCall :: String -> [SplExpr] -> SplTypeCheckResult
-checkFunctionCall name args = do
-    funcType <- gets (Map.lookup name)
-    case funcType of
-        Nothing -> fail $ "Undeclared function name " ++ name
-        Just (SplSimple _) -> fail "Expected function, got variable"
-        Just (SplTypeFunction argTypes retType) -> (do
-                when (length args /= length argTypes) (
-                        fail ("Got " ++ (show . length) args ++
-                              " arguments but expected " ++ (show . length) argTypes))
-                argTypeResults <- mapM typeCheck args
-                let result = all checkArgType (zip argTypes argTypeResults)
-                if result
-                    then returnSimple retType
-                    else fail ("Error checking arguments for " ++ name ++ ":\n"
-                                 ++ "Expected: " ++ (intercalate ", " (map show args)) ++ "\n"
-                                 ++ "Got:      " ++ (intercalate ", " (map show argTypes)))
-
-            )
-    where
-        checkArgType :: (SplSimpleTypeR, SplTypeR) -> Bool
-        checkArgType (expected, SplSimple actual) = actual == expected && notVoid actual
-        checkArgType _ = False
-
-typeCheckBinOp :: SplBinaryOperator -> SplSimpleTypeR -> SplTypeCheckResult
-typeCheckBinOp SplOperatorAdd t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorAdd t@(SplTypeConst SplChar) = returnSimple t
-
-typeCheckBinOp SplOperatorSubtract t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorSubtract t@(SplTypeConst SplChar) = returnSimple t
-
-typeCheckBinOp SplOperatorMultiply t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorDivide t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorModulus t@(SplTypeConst SplInt) = returnSimple t
-
-typeCheckBinOp SplOperatorLess t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorLess t@(SplTypeConst SplChar) = returnSimple t
-
-typeCheckBinOp SplOperatorLessEqual t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorLessEqual t@(SplTypeConst SplChar) = returnSimple t
-
-typeCheckBinOp SplOperatorEqual t@(SplTypeConst _) = returnSimple t
-typeCheckBinOp SplOperatorEqual t@(SplTypeTupleR _ _) = returnSimple t
-typeCheckBinOp SplOperatorEqual t@(SplTypeListR _) = returnSimple t
-
-typeCheckBinOp SplOperatorGreaterEqual t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorGreaterEqual t@(SplTypeConst SplChar) = returnSimple t
-
-typeCheckBinOp SplOperatorGreater t@(SplTypeConst SplInt) = returnSimple t
-typeCheckBinOp SplOperatorGreater t@(SplTypeConst SplChar) = returnSimple t
-
-typeCheckBinOp SplOperatorNotEqual t@(SplTypeConst _) = returnSimple t
-typeCheckBinOp SplOperatorNotEqual t@(SplTypeTupleR _ _) = returnSimple t
-typeCheckBinOp SplOperatorNotEqual t@(SplTypeListR _) = returnSimple t
-
-typeCheckBinOp SplOperatorAnd t@(SplTypeConst SplBool) = returnSimple t
-typeCheckBinOp SplOperatorOr t@(SplTypeConst SplBool) = returnSimple t
-
-typeCheckBinOp SplOperatorCons _ = fail "Don't use typeCheckBinOp on Cons, dummy"
-typeCheckBinOp o t = fail $ "Unsupported operands of type " ++ (show t) ++ " for operator " ++ (show o)
--}
