2ec165c251cb57349b34093e20b8239ee426d713
Thom Wiggers <thom@thomwiggers.nl>
2017-05-25 14:02:28 +0200

    start on aarch64 target

---
+++ splc/src/SplAArch64.hs
@@ -0,0 +1,54 @@
+module SplAArch64 where
+
+import           Prelude hiding (EQ, GT, LT)
+
+type Offset = Int
+type Size = Int
+type Label = String
+
+data Register = X Int | FP | LR | SP | PR String | Imm Int
+    deriving Eq
+
+data Address = Address Register Offset
+    deriving Eq
+
+instance Show Address where
+    show (Address r o) = "[" ++ show r ++ ", #" ++ show o ++ "]"
+
+data Condition = Negative | Carry | Overflow | Zero
+    deriving (Show, Eq)
+
+instance Show Register where
+    show (X a) = if a < 0 || a > 28
+                    then fail "BAD REGISTER x" ++ show a
+                    else "x" ++ show a
+    show (PR s) = "PR '" ++ s ++ "'"
+    show (Imm i) = "#" ++ show i
+    show FP = "fp"
+    show LR = "lr"
+    show SP = "sp"
+
+-- FIXME encode MOD using MSUB
+
+data AArch64Instruction
+    --    Rd       Rn       Rm        Ra
+    = ADD Register Register Register
+    | MUL Register Register Register
+    | SDIV Register Register Register
+    | SUB Register Register Register
+    | NEG Register Register
+    | MSUB Register Register Register Register -- Rd = Ra - Rn * Rm (note Ra is last register!)
+    | AND Register Register Register
+    | OR Register Register Register
+    | XOR Register Register Register
+    | CMP Register Register
+    | BranchConditional Condition Label
+    | BranchLink Label
+    | LDR Register Address      -- ldr x0, [sp, #16]
+    | STR Register Address      -- str x0, [sp, #12]
+    | RET
+    | NOP
+    | Comment String
+    | Label Label
+    | BasicBlock [AArch64Instruction]
+    deriving (Show, Eq)
--- splc/src/SplIRtoAArch64.hs
+++ splc/src/SplIRtoAArch64.hs
@@ -1,5 +1,7 @@
 module SplIRtoAArch64 where
 
+import SplAST (SplBinaryOperator(..), SplUnaryOperator(..))
+import SplIR
 import SplAArch64
 import Control.Monad.Writer
 import Control.Monad.State
@@ -9,3 +11,36 @@ newtype AArch64State = AArch64State {
     }
 
 type IRtoAArch64State = WriterT [AArch64Instruction] (State AArch64State) ()
+
+out :: AArch64Instruction -> IRtoAArch64State
+out x = tell [x]
+
+
+programToAArch64 :: SplIR -> IRtoAArch64State
+programToAArch64 ir = mapM_ toAArch64 ir
+
+
+toAArch64 :: SplInstruction -> IRtoAArch64State
+-- binary operations
+toAArch64 (SplBinaryOperation SplOperatorAdd (Reg rd) (Reg rn) (Reg rm))
+    = out $ ADD (PR rd) (PR rn) (PR rm)
+toAArch64 (SplBinaryOperation SplOperatorSubtract (Reg rd) (Reg rn) (Reg rm))
+    = out $ SUB (PR rd) (PR rn) (PR rm)
+toAArch64 (SplBinaryOperation SplOperatorMultiply (Reg rd) (Reg rn) (Reg rm))
+    = out $ MUL (PR rd) (PR rn) (PR rm)
+toAArch64 (SplBinaryOperation SplOperatorDivide (Reg rd) (Reg rn) (Reg rm))
+    = out $ SDIV (PR rd) (PR rn) (PR rm)
+-- FIXME rest of binops
+
+-- unary operations:
+toAArch64 (SplUnaryOperation SplOperatorInvert (Reg rd) (Reg rm))
+    = out $ XOR (PR rd) (PR rm) (Imm 1)
+toAArch64 (SplUnaryOperation SplOperatorNegate (Reg rd) (Reg rm))
+    = out $ NEG (PR rd) (PR rm)
+
+-- functions
+toAArch64 (SplFunction label _args ir) = do
+    out $ Label label
+    out $ Comment "FIXME args"
+    mapM_ toAArch64 ir
+    out RET
