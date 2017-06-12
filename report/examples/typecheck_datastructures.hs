72002d226050d5f67fc4eea94e3833c79c26fc1c
Thom Wiggers <thom@thomwiggers.nl>
2017-06-12 14:48:10 +0200

    initial work on type checking section

---
+++ report/examples/typecheck_datastructures.hs
@@ -0,0 +1,20 @@
+type Environment 
+  = Map.Map String SplTypeR
+type SplTypeCheckResult 
+  = StateT Environment (Either String) SplTypeR
+
+data SplSimpleTypeR 
+  = SplTypeConst SplBasicType
+  | SplTypeTupleR SplSimpleTypeR SplSimpleTypeR
+  | SplTypeListR SplSimpleTypeR
+  | SplTypeVar Int
+  | SplVoid
+  deriving (Show, Eq)
+
+data SplTypeR 
+  = SplSimple SplSimpleTypeR
+  | SplTypeFunction [SplSimpleTypeR] SplSimpleTypeR
+  deriving (Show, Eq)
+
+class SplTypeChecker a where
+  typeCheck :: a -> SplTypeCheckResult
---
+++ report/examples/typecheck_unaryops.hs
@@ -0,0 +1,9 @@
+typeCheck (SplUnaryExpr op expr) = do
+  exprType <- typeCheck expr >>= unsimple >>= disallowVoid
+  case op of
+    SplOperatorInvert -> if (exprType == SplTypeConst SplBool)
+      then returnSimple exprType
+      else fail "Cannot invert a non-Bool"
+    SplOperatorNegate -> if (exprType == SplTypeConst SplInt)
+      then returnSimple exprType
+      else fail "Cannot invert a non-Int"
